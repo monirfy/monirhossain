a917573047b6e88c21b6902f455ed8e01ec4bdc3
Monir H <monirhfy@gmail.com>
2021-03-14 22:20:37 +0600

    Header title

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -7,7 +7,7 @@ const Promo = () => {
         <div className='row'>
           <div className='col-lg-6 mx-auto'>
             <h1>Welcome to My Portfolio.</h1>
-            <p className='lead'>Hi there, I am Monir Hossain. I build websites that are fast, secure and ease to mange</p>
+            <p className='lead'>Hi there, I am Monir Hossain. I build websites that are fast, secure and ease to mange.</p>
           </div>
         </div>
       </div>
