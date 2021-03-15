cf052be9ae8a9ddc85edb29ed046948019979987
monirfy <me@monirh.com>
2021-03-15 23:24:04 +0600

    react routing link at header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../images/monirhossain.svg';
+import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
   return (
@@ -8,19 +9,19 @@ const Header = () => {
         <div className='row'>
           <div className='col d-flex align-items-center justify-content-between'>
             <div className='logo'>
-              <a href='/'>
+              <a as={Link} to='/'>
                 <img src={logo} alt='' />
               </a>
             </div>
             <div className='main-menu'>
               <ul>
                 <li>
-                  <a className='active' href='/'>
+                  <a className='active' as={Link} to='/'>
                     Home
                   </a>
                 </li>
                 <li>
-                  <a href='about/'>About Me</a>
+                  <a as={Link} to='/about'>About Me</a>
                 </li>
               </ul>
             </div>
