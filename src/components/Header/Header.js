e05c22d1950d833cd53e994fa967982122f7afcd
monirfy <me@monirh.com>
2021-03-16 09:07:01 +0600

    Update header js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,14 +9,6 @@ const Header = () => {
         <div className='row'>
           <div className='col d-flex align-items-center justify-content-between'>
             <div className='logo'>
-<<<<<<< HEAD
-              {/* <a as={Link} to='/' rel='noreferrer'>
-=======
-              <a as={Link} to='/'>
->>>>>>> 5558995a44c798117d35ebc3b95972f2772d6739
-                <img src={logo} alt='' />
-              </a> */}
-
               <Link to='/'>
                 <img src={logo} alt='' />
               </Link>
@@ -24,19 +16,10 @@ const Header = () => {
             <div className='main-menu'>
               <ul>
                 <li>
-<<<<<<< HEAD
                   <Link to='/'>Home</Link>
                 </li>
                 <li>
                   <Link to='/about'>About</Link>
-=======
-                  <a className='active' as={Link} to='/'>
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a as={Link} to='/about'>About Me</a>
->>>>>>> 5558995a44c798117d35ebc3b95972f2772d6739
                 </li>
               </ul>
             </div>
